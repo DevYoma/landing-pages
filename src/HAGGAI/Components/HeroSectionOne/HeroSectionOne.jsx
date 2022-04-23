b9bab4c73422a705d8c94d6518c0066fe0434642
DevYoma <Lawrenceyoma@gmail.com>
2022-04-23 16:27:13 +0100

    done with the flexDirection and translation prop

--- src/HAGGAI/Components/HeroSectionOne/HeroSectionOne.jsx
+++ src/HAGGAI/Components/HeroSectionOne/HeroSectionOne.jsx
@@ -3,17 +3,34 @@ import './HeroSectionOne.css'
 import Phone from './assets/phone.png'
 import Polygon from './assets/polygon.png'
 import Icon1 from './assets/Icon1.svg'
+import PinkCircle from './assets/pinkCircle.png'
 
-const HeroSectionOne = () => {
+const HeroSectionOne = ({ flexDirection, rotate }) => {
+    // console.log(flexDirection)
+    console.log(rotate)
+    let translate;
+    switch (rotate) {
+        case rotate = "left":
+            translate="heroSectionOne__left leftTranslate"
+            break;
+        case rotate = "right":
+            translate="heroSectionOne__left rightTranslate"
+            break;
+        case rotate = "normal":
+            translate = "heroSectionOne__left"
+        default:
+            break;
+    }
   return (
-    <div className='heroSectionOne haggai__container'>
-        <div className='heroSectionOne__left'>
+    <div className={flexDirection ? 'heroSectionOne haggai__container heroSectionOne__flexDirection' : 'heroSectionOne haggai__container'}>
+        <div className={translate}>
             <img src={Polygon} alt="polygon" />
             <img src={Phone} alt="phone" />
         </div>
         <div className='heroSectionOne__right'>
             <div className='heroSectionOne__rightIcon'>
-                {/* <img src={Icon1} alt="icon1" /> */}
+                <img src={Icon1} alt="icon1" />
+                <img src={PinkCircle} alt="pinkCircle" />
             </div>
 
             <div className="heroSectionOne__rightHeading">
